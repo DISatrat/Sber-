b7f5914e54011b2f5c68d19e38f49de3b2fedc2b
Дмитрий Малашенко <dimaneopal@mail.ru>
2022-12-25 16:38:16 +0300

    Sber Files

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,15 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) throws IOException {
+
+
+
 
         String file2 = "src/main/java/com/example/demo/city_ru1.csv";
         Path path2 = Paths.get(file2);
@@ -58,14 +58,38 @@ public class DemoApplication {
 //            System.out.println(cities2.get(i));
 //        }
 
-        
-        SpringApplication.run(DemoApplication.class, args);
-    }
+        //максимальное число жителей
+
+        ArrayList<City>cities2=new ArrayList<>();
+        while (scanner1.hasNext()){
+            City city=CreateCity(scanner1.nextLine());
+            cities2.add(city);
+        }
+
+        int max=0;
+        int maxId=0;
+
+        for (int i = 0; i < cities2.size(); i++) {
+            int pop=Integer.parseInt(cities2.get(i).population);
+            if(pop>max){
+            max=pop;
+                maxId=Integer.parseInt(cities2.get(i).id)-1;
+            }
+
+
+        }
+        System.out.println("["+maxId+"]"+" = "+max);
+
+
+        SpringApplication.run(DemoApplication .class,args);
+}
 
     private static City CreateCity(String line) {
 
         String[] ArrayLines;
         ArrayLines = line.split(";");
+
+        String id=ArrayLines[0];
 
         String name = ArrayLines[1];
         String region = ArrayLines[2];
@@ -76,17 +100,45 @@ public class DemoApplication {
             foundation = ArrayLines[5];
         }
 
-        return new City(name,region,district,population,foundation);
+        return new City(id,name,region,district,population,foundation);
+    }
+    public static void PopulousCity() throws IOException {
+
+        String file2 = "src/main/java/com/example/demo/city_ru1.csv";
+        Path path2 = Paths.get(file2);
+        Scanner scanner1 = new Scanner(path2);
+        String[] Cities;
+        while(scanner1.hasNext()){
+
+        }
+
+//        int max=0;
+//
+//        while (scanner1.hasNext()){
+//            String line2=scanner1.nextLine();
+//            String[]lines;
+//            lines=line2.split(";");
+//
+//            int pop=Integer.parseInt(lines[4]);
+//
+//            if(pop>max){
+//                max=pop;
+//                System.out.println("["+id+"]"+" = "+max);
+//            }
+//
+//        }
     }
 
     public static class City extends ArrayList<City> {
+        private String id;
         private String name;
         private String region;
         private String district;
         private String population;
         private String foundation;
 
-        private City(String name, String region, String district, String population, String foundation) {
+        private City(String id,String name, String region, String district, String population, String foundation) {
+            this.id=id;
             this.name = name;
             this.region = region;
             this.district = district;
@@ -136,8 +188,8 @@ public class DemoApplication {
 
         @Override
         public String toString() {
-            return "City{name='" + this.name + "', region='" + this.region + "', district='" + this.district + "', population=" + this.population + ", foundation='" + this.foundation + "'}";
-
+            //return "City{name='" + this.name + "', region='" + this.region + "', district='" + this.district + "', population=" + this.population + ", foundation='" + this.foundation + "'}";
+            return this.id+" "+this.name+" "+this.region+" "+this.district+" "+this.population+" "+this.foundation;
         }
     }
 }
