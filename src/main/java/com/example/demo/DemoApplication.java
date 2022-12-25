b3c25c9ebf244f668f08f5451d080a3a13cfe5c8
Дмитрий Малашенко <dimaneopal@mail.ru>
2022-12-25 17:03:13 +0300

    Sber Files

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -60,25 +60,46 @@ public class DemoApplication {
 
         //максимальное число жителей
 
+//        ArrayList<City>cities2=new ArrayList<>();
+//        while (scanner1.hasNext()){
+//            City city=CreateCity(scanner1.nextLine());
+//            cities2.add(city);
+//        }
+//
+//        int max=0;
+//        int maxId=0;
+//
+//        for (int i = 0; i < cities2.size(); i++) {
+//            int pop=Integer.parseInt(cities2.get(i).population);
+//            if(pop>max){
+//            max=pop;
+//                maxId=Integer.parseInt(cities2.get(i).id)-1;
+//            }
+//
+//
+//        }
+//        System.out.println("["+maxId+"]"+" = "+max);
+
+        //количесто городов в регионах
+
         ArrayList<City>cities2=new ArrayList<>();
         while (scanner1.hasNext()){
             City city=CreateCity(scanner1.nextLine());
             cities2.add(city);
         }
 
-        int max=0;
-        int maxId=0;
 
-        for (int i = 0; i < cities2.size(); i++) {
-            int pop=Integer.parseInt(cities2.get(i).population);
-            if(pop>max){
-            max=pop;
-                maxId=Integer.parseInt(cities2.get(i).id)-1;
+        int c=1;
+        for (int i = 0; i < cities2.size()+1; i++) {
+            if(cities2.get(i+1).region.equals(cities2.get(i).region)){
+                c++;
+
             }
-
-
+            else {
+                System.out.println(cities2.get(i).region+" - "+c);
+                c=1;
+            }
         }
-        System.out.println("["+maxId+"]"+" = "+max);
 
 
         SpringApplication.run(DemoApplication .class,args);
@@ -101,32 +122,6 @@ public class DemoApplication {
         }
 
         return new City(id,name,region,district,population,foundation);
-    }
-    public static void PopulousCity() throws IOException {
-
-        String file2 = "src/main/java/com/example/demo/city_ru1.csv";
-        Path path2 = Paths.get(file2);
-        Scanner scanner1 = new Scanner(path2);
-        String[] Cities;
-        while(scanner1.hasNext()){
-
-        }
-
-//        int max=0;
-//
-//        while (scanner1.hasNext()){
-//            String line2=scanner1.nextLine();
-//            String[]lines;
-//            lines=line2.split(";");
-//
-//            int pop=Integer.parseInt(lines[4]);
-//
-//            if(pop>max){
-//                max=pop;
-//                System.out.println("["+id+"]"+" = "+max);
-//            }
-//
-//        }
     }
 
     public static class City extends ArrayList<City> {
